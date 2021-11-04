154ef16109b038eb2b39c6d1af246dc84eaf7419
Bulavsky Igor <ibulavsky@gmail.com>
2021-11-04 12:31:39 +0300

    Updates

---
+++ precache-manifest.b1644833b035a0a49754f1be43aba443.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed3c5ee576640f49070bf79053ba5e89",
+    "url": "/Game-StarWars/index.html"
+  },
+  {
+    "revision": "8de2067919a3837a5a49",
+    "url": "/Game-StarWars/static/css/main.9f058c27.chunk.css"
+  },
+  {
+    "revision": "866e82116c9662976ea5",
+    "url": "/Game-StarWars/static/js/2.e1040b31.chunk.js"
+  },
+  {
+    "revision": "8de2067919a3837a5a49",
+    "url": "/Game-StarWars/static/js/main.115ea13b.chunk.js"
+  },
+  {
+    "revision": "76df8680d3dbb532f3d3",
+    "url": "/Game-StarWars/static/js/runtime-main.5a09e601.js"
+  },
+  {
+    "revision": "ae2b62e443a35f19e3a6a54dbdcda615",
+    "url": "/Game-StarWars/static/media/yoda.ae2b62e4.png"
+  },
+  {
+    "revision": "2bb2c9de39de8d5728c282f54962e706",
+    "url": "/Game-StarWars/static/media/yoda2.2bb2c9de.png"
+  }
+]);
